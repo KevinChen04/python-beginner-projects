8450dd82be95ed168cc1da23d4d4f35d03c1004d
KevinChen04 <113963798+KevinChen04@users.noreply.github.com>
2022-09-27 10:43:43 -0800

    Update main.py

--- projects/Dice-Roll-Simulator/main.py
+++ projects/Dice-Roll-Simulator/main.py
@@ -4,7 +4,7 @@ while True:
     print(" 1. roll the dice \n  2. exit  ")
     user = int(input("what you want to do\n"))
     if user == 1:
-        number = random.randint(1, 6)
+        number = random.randint(1, 20)
         print(number)
     else:
         break
